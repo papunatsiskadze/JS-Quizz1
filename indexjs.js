c8848ad08cb456c666e4c1ca179d07fed0abcf7f
papunatsiskadze <papunaciskadze@gmail.com>
2022-05-24 14:16:18 +0400

    done

---
+++ indexjs.js
@@ -0,0 +1,16 @@
+var salary = prompt('თქვენი ანაზღაურება');
+if (salary>=1000){
+    alert("თქვენი ანაზღაურება მაღალია")
+}
+else if (salary){
+    alert("თქვენი ანაზღაურება საშუალოა")
+}
+
+var people=[
+           ["Tom", 25,false],
+           ["bill", 38,true],
+           ["Alice",23,false]
+];
+console.log(people [0][2]);
+console.log(people [1][1]);
+console.log(people [2][2]);
